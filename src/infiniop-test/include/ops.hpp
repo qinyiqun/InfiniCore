a215f7a0049c6125f953b5a023c1c7a81be033c7
tianyuxbear <tianyuxbear@gmail.com>
2025-07-30 18:42:53 +0800

    issue/456/feat: add gelu_backward operator

--- src/infiniop-test/include/ops.hpp
+++ src/infiniop-test/include/ops.hpp
@@ -32,6 +32,7 @@ DECLARE_INFINIOP_TEST(logical_or)
 DECLARE_INFINIOP_TEST(equal)
 DECLARE_INFINIOP_TEST(relu_backward)
 DECLARE_INFINIOP_TEST(gelu)
+DECLARE_INFINIOP_TEST(gelu_backward)
 
 #define REGISTER_INFINIOP_TEST(name)                      \
     {                                                     \
@@ -70,6 +71,7 @@ DECLARE_INFINIOP_TEST(gelu)
         REGISTER_INFINIOP_TEST(equal)               \
         REGISTER_INFINIOP_TEST(relu_backward)       \
         REGISTER_INFINIOP_TEST(gelu)                \
+        REGISTER_INFINIOP_TEST(gelu_backward)       \
     }
 
 namespace infiniop_test {
