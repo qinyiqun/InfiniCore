13c286872311007a4236e88fc410d9fa80aa518b
PPPoint-t <1024879159@qq.com>
2025-09-16 16:32:55 +0800

    add exp operator

--- scripts/python_test.py
+++ scripts/python_test.py
@@ -24,6 +24,7 @@ def run_tests(args):
         "rope.py",
         "sub.py",
         "swiglu.py",
+        "exp.py",
     ]:
         result = subprocess.run(
             f"python {test} {args} --debug", text=True, encoding="utf-8", shell=True
