9e3c3a329bcc174e922cb011455531c313ead285
PPPoint-t <1024879159@qq.com>
2025-09-17 09:37:50 +0800

    add where operator

--- include/infiniop.h
+++ include/infiniop.h
@@ -19,6 +19,7 @@
 #include "infiniop/ops/sub.h"
 #include "infiniop/ops/swiglu.h"
 #include "infiniop/ops/topkrouter.h"
+#include "infiniop/ops/where.h"
 #include "infiniop/tensor_descriptor.h"
 
 #endif // __INFINIOP_API_H__
---
+++ src/infiniop/ops/where/metax/where_metax.h
@@ -0,0 +1,8 @@
+#ifndef __WHERE_METAX_API_H__
+#define __WHERE_METAX_API_H__
+
+#include "../../../elementwise/metax/elementwise_metax_api.h"
+
+ELEMENTWISE_DESCRIPTOR(where, metax)
+
+#endif // __WHERE_METAX_API_H__
