69aeb449d6f5bbb4a6a7c7932e1c0036284ade1e
S-hhhhh <2320230838@mail.nankai.edu.cn>
2025-09-17 16:59:50 +0800

    issue/456/feat : add reduce_max operator

---
+++ src/infiniop/ops/reduce_max/cpu/reduce_max_cpu.h
@@ -0,0 +1,7 @@
+#ifndef __REDUCE_MAX_CPU_H__
+#define __REDUCE_MAX_CPU_H__
+#include "../reduce_max.h"
+
+DESCRIPTOR(cpu)
+
+#endif
---
+++ src/infiniop/ops/reduce_max/nvidia/reduce_max_nvidia.cuh
@@ -0,0 +1,8 @@
+#ifndef __REDUCE_MAX_NVIDIA_H__
+#define __REDUCE_MAX_NVIDIA_H__
+
+#include "../reduce_max.h"
+
+DESCRIPTOR(nvidia)
+
+#endif
